4e4c72561f9b0a0ed262d8e3539cc11a8aea7a95
zero2603 <20153713@student.hust.edu.vn>
2018-08-16 12:56:16 +0700

    fix conflict

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,26 +17,9 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-<<<<<<< HEAD
         if (Auth::guard($guard)->check()) {
             if(Auth::user()->is_admin == 1) return redirect('/admin');
             return redirect('/home');
-=======
-        switch ($guard) {
-            case 'admin': {
-                if(Auth::guard($guard)->check()){
-                    return redirect()->route('admin');
-                }
-                break;
-            }
-            
-            default: { 
-                if (Auth::guard($guard)->check()) {
-                    return redirect('/home');
-                }
-                break;
-            }
->>>>>>> bcfc43ec857c6a5cea610b4c0316b025a28968d8
         }
 
         return $next($request);
