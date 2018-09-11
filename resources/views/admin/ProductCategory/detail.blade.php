3a7fd8d0c433345813cbe986369b364ac83f8042
vantu19000 <vantu19000@gmail.com>
2018-09-11 23:47:56 +0700

    product 2

--- resources/views/admin/ProductCategory/detail.blade.php
+++ resources/views/admin/ProductCategory/detail.blade.php
@@ -1,54 +1,37 @@
+<?php
+//echo "<pre>";
+//print_r($item->description);
+//die;
+?>
+
 @extends('layouts.admin')
 
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <h4 class="m-b-20 header-title">Nhóm tỉnh</h4>
-                <form method="post" action="{{url('admin?controller=ProvinceGroup&task=update&id='.$item->id)}}" class="form-horizontal">
-
+            <div class="col-sm-12">
+                <h4 class="m-b-20 header-title">Danh mục sản phẩm</h4>
+                <form enctype='multipart/form-data' method="POST" action="{{url('admin?controller=ProductCategory&task=update&id='.$item->id)}}">
                     {{ csrf_field() }}
+                    <div class="col-sm-12">
 
                         <div class="form-group">
-                            <label>Tên nhóm <span>*</span></label>
-                            <input type="text" value="{{$item->name}}" class="form-control" name="data[name]" required />
+                            <label>Tên danh mục <span>*</span></label>
+                            <input type="text" class="form-control" name="data[name]" value="{{$item->name}}" required />
                         </div>
 
-                        @foreach($province AS $value)
+                        <div class="form-group">
+                            <label>Mô tả ngắn <span>*</span></label>
+                            <textarea type="text" class="form-control" name="data[description]" required>{{$item->description}}</textarea>
+                        </div>
 
-
-                            <div class="checkbox checkbox-custom">
-                                <input
-                                        id="province_{{$value->matp}}"
-
-                                        @foreach(json_decode($item->province_ids) AS $val)
-
-                                            @if($val == $value->matp)
-                                                checked
-                                            @endif
-
-                                        @endforeach
-
-                                        type="checkbox" name="data[province_ids][]" value="{{$value->matp}}">
-                                <label for="province_{{$value->matp}}">
-                                    {{$value->name}}
-                                </label>
-
-                            </div>
-
-
-                        @endforeach
-
-
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                        <button type="reset" class="btn btn-dark">Reset</button>
                     </div>
 
-
+                    <div class="col-sm-12">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
                 </form>
             </div>
-
         </div>
     </div>
 @endsection
