6e62e2182e643a1d8eec108ebddbe0ec41566184
zero2603 <20153713@student.hust.edu.vn>
2018-09-13 00:40:45 +0700

    fix conflict

--- resources/assets/js/routes/index.js
+++ resources/assets/js/routes/index.js
@@ -28,25 +28,18 @@ const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
 
 class MainApp extends Component {
 	render() {
-		console.log("this is route");
 		return (
 			<div className="App">
 				<Header></Header>
 				<div className="header-spacer"></div>
 				<Switch>
-<<<<<<< HEAD
 					{/* Home */}
 					<AppRoute exact path="/" layout={MainLayout} component={Home} />
 					{/* Profile */}
 					<AppRoute exact path="/profile/:id" layout={SecondLayout} component={UserProfile} />
 					<AppRoute exact path="/profile/:id/setting" layout={SecondLayout} component={UserSetting} />
-=======
-					<AppRoute exact path="/" layout={MainLayout} component={NewFeeds} />
-					<AppRoute exact path="/country" layout={SecondLayout} component={Country} />
-					<AppRoute exact path="/profile/:id" layout={SecondLayout} component={Profile} />
-					<AppRoute exact path="/profile/:id/setting" layout={SecondLayout} component={UserDetail} />
+					{/* Cafe */}
 					<AppRoute exact path="/cafe" layout={MainLayout} component={Cafe} />
->>>>>>> 5d9f661bfc455a09ae64b482ad380e35b539b9ce
 				</Switch>
 			</div>
 		);
