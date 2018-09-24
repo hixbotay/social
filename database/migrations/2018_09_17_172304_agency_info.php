d6dbc25044359c06f9de1f83fad71dea67ad5afe
vuong anh duong <vuonganhduong812@gmail.com>
2018-09-24 11:51:53 +0700

    sua file migrate

--- database/migrations/2018_09_17_172304_agency_info.php
+++ database/migrations/2018_09_17_172304_agency_info.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAgencyInfo extends Migration
+class AgencyInfo extends Migration
 {
     /**
      * Run the migrations.
