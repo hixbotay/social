5d925213aab8f230af86b2b5cb353b0889ad2100
zero2603 <20153713@student.hust.edu.vn>
2018-10-22 00:15:29 +0700

    test register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,6 +71,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // print_r($data);
         return User::create([
             'name' => $data['name'],
             'mobile' => $data['mobile'],
