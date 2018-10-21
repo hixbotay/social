665f4f2040edc91163c3cf43e4cccba1d51909b7
vantu19000 <vantu19000@gmail.com>
2018-10-21 23:24:06 +0700

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,22 +39,25 @@
                 <div class="col-sm-4">
                     <select class="form-control" name="province_id" id="register-province" required>
                         <option>Tỉnh/TP</option>
+                        @foreach(App\ProvinceGroup::all_province() AS $value)
+                            <option value="{{$value->matp}}">{{$value->name}}</option>
+                        @endforeach
                         {{-- @foreach ($provinces as $item)
                             <option value={{$item->matp}}>{{$item->name}}</option>
                         @endforeach --}}
                     </select>
                 </div>
                 <div class="col-sm-4">
-                    <select class="form-control" name="district_id" required>
-                        <option>Huyện/Quận</option>
+                    <select class="form-control" name="district_id" id="district" required>
+                        <option value="">Huyện/Quận</option>
                         {{-- @foreach ($districts as $item)
                             <option value={{$item->maqh}}>{{$item->name}}</option>
                         @endforeach --}}
                     </select>
                 </div>
                 <div class="col-sm-4">
-                    <select class="form-control" name="village_id" required>
-                        <option>Xã/Phường</option>
+                    <select class="form-control" name="village_id" id="village" required>
+                        <option value="">Xã/Phường</option>
                         {{-- @foreach ($villages as $item)
                             <option value={{$item->xaid}}>{{$item->name}}</option>
                         @endforeach --}}
@@ -93,40 +96,46 @@
 
 <script>
     var districts = [];
-    // $('#register-province').change(function() {
-    //     console.log(1111111);
-        // $.getJSON('districts/' + $('#register-province').find(":selected").val(), function(data) {
-        //     // var items = [];
-        //     // $.each( data, function( key, val ) {
-        //     //     items.push(value);
-        //     // });
-        //     console.log(data);
-        // }).fail(function (jqxhr, status, error) { 
-        //     console.log('error', status, error) }
-        // );
-        // $.ajaxSetup({
-        //     headers: {
-        //         'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-        //     }
-        // });
-    //     jQuery.ajax({
-    //         url: 'districts/' + $('#register-province').find(":selected").val(),
-    //         type: 'GET',
-    //         dataType: "json",
-    //         data: {districts: districts},
-    //         // contentType: 'application/json; charset=utf-8',
-    //         // success: function(data) {
-    //         //     console.log(data);
-    //         // },
-    //         // error: function (jqxhr, status, error) { 
-    //         //     console.log('error', status, error) 
-    //         // }
-    //     }).done(function(data) {
-    //         console.log(data);
-    //     }).fail(function (jqxhr, status, error) { 
-    //         console.log('error', status, error) 
-    //     });
-    // })
+    $('#register-province').change(function() {
+        $.getJSON('api/districts/' + $('#register-province').find(":selected").val(), function(data) {
+            // var items = [];
+            // $.each( data, function( key, val ) {
+            //     items.push(value);
+            // });
+            for (let i = 0; i < data.length; i ++){
+                jQuery("#district").append('<option value="'+data[i].maqh+'">'+data[i].name+'</option>');
+            }
+            console.log(data);
+        }).fail(function (jqxhr, status, error) {
+            console.log('error', status, error) }
+        );
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+            }
+        });
+        jQuery.ajax({
+            url: 'api/districts/' + $('#register-province').find(":selected").val(),
+            type: 'GET',
+            dataType: "json",
+            data: {districts: districts},
+        }).done(function(data) {
+            console.log(data);
+        }).fail(function (jqxhr, status, error) {
+            console.log('error', status, error)
+        });
+    })
+
+    $('#district').change(function() {
+        $.getJSON('api/communes/' + $('#district').find(":selected").val(), function(data) {
+            for (let i = 0; i < data.length; i ++){
+                jQuery("#village").append('<option value="'+data[i].maqh+'">'+data[i].name+'</option>');
+            }
+            console.log(data);
+        }).fail(function (jqxhr, status, error) {
+            console.log('error', status, error) }
+        );
+    })
 </script>
 
 @endsection
