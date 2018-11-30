1e287e63b9d6b94890744c48c1017e5741ad53a2
vantu19000 <vantu19000@gmail.com>
2018-11-30 16:27:12 +0700

    check vip function

--- app/Payments.php
+++ app/Payments.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Object_;
 
 class Payments extends Model
 {
@@ -21,8 +23,43 @@ class Payments extends Model
         'pay_number'
     ];
 
-    public function checkVIP(){
-        $userID = Auth::id();
+    public static function checkVIP($id){
+
+        $now = date("Y-m-d H:i:s");
+
+        $result = Payments::where('pay_type', 'VIP')->where('pay_status', 1)
+            ->where(function($query) use ($now) {
+                $query->orWhere(function($query) use ($now) {
+                    $query->where('from_time', '<=', $now);
+                    $query->where('to_time', '>=', $now);
+                });
+                $query->orWhere(function($query) use ($now) {
+                    $query->orWhere('from_time', '>=', $now);
+                });
+            })
+            ->where('user_id', '=', $id)
+            ->orderBy('to_time', 'DESC')
+            ->get();
+
+        $vip = false;
+        foreach ($result AS $value){
+
+            $from = new \DateTime($value->from_time);
+            $to = new \DateTime($value->to_time);
+            $current = new \DateTime($now);
+            if ($from <= $current && $current <= $to) {
+                $vip = true;
+                break;
+            }
+
+        }
+
+        $vipData = array();
+        $vipData['vip'] = $vip;
+        $vipData['expire'] = $result[0]->to_time;
+
+        return (object)$vipData;
+
     }
 
 }
