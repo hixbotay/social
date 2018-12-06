e70373dcd1f42ba2395193cce9fbbecbb3ac020d
vantu19000 <vantu19000@gmail.com>
2018-12-06 22:07:09 +0700

    history

--- resources/assets/js/routes/payment/history.js
+++ resources/assets/js/routes/payment/history.js
@@ -50,7 +50,7 @@ class PaymentHistory extends Component {
                         </div>
                     </div>
                     <div className="col-md-3 action">
-                        <div className={"row"} onClick={() => {
+                        <div className={"row middle-vertical"} onClick={() => {
                             alert("Dang hoan thien");
                             console.log(this.props)
                         }}>
@@ -86,21 +86,76 @@ class PaymentHistory extends Component {
                 <div className="mb-3"></div>
 
                 <div className={"row historyBox"}>
+
                     <div className={"col-md-12 title"}>
-                        Hom nay
+                        <div className={"row text-center"}>
+                            <div className={"col-md-6 text-center row-right"}>
+                                <p>Từ ngày</p>
+                                <p align="center">
+                                    <input type={'date'} name={'from_date'} onChange={() => {}} />
+                                </p>
+                            </div>
+                            <div className={"col-md-6 text-center row-left"}>
+                                <p>Đến ngày</p>
+                                <p align="center">
+                                    <input type={'date'} name={'to_date'} onChange={() => {
+
+                                    }} />
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className={"row"}>
+                            <div className={"col-md-12 text-center"}>
+                                <button type="button" className="custombutton1">Tìm kiếm</button>
+                            </div>
+                        </div>
+
                     </div>
+
+
                     <div className={"col-md-12 content"}>
-                        <div className="row">
+
+                        <div className={"row paymentheading center text-center"}>
+                            <ul className={"list-inline"}>
+                                <li>Tat ca</li>
+                                <li>Tien vao</li>
+                                <li>Tien ra</li>
+                            </ul>
+                        </div>
+
+                        <div className="row item">
                             <div className={"col-md-8"}>
                                 <div className={"row"}>
                                     <div className="col-md-4 text-right">
-                                        <i className="fab fa-500px"></i>
+                                        <img src={"https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color-shapes/128/heart-circle-red-2-512.png"} />
                                     </div>
                                     <div className="col-md-8">
 
-                                        <p>Nang cap tai khoan vip</p>
-                                        <p>15h 15p</p>
-                                        <p>Viettinbank</p>
+                                        <h6>Nang cap tai khoan vip</h6>
+                                        <h6>15h 15p</h6>
+                                        <h6>Viettinbank</h6>
+                                        <h6 className={"text-success"}>Done</h6>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={"col-md-4"}>
+                                <p className="text-success">+ 300.000 VND</p>
+                            </div>
+                        </div>
+                        <div className="row item">
+                            <div className={"col-md-8"}>
+                                <div className={"row"}>
+                                    <div className="col-md-4 text-right">
+                                        <img src={"https://cdn3.iconfinder.com/data/icons/cash-card-starters-glyph/48/Sed-08-512.png"} />
+                                    </div>
+                                    <div className="col-md-8">
+
+                                        <h6>Nang cap tai khoan vip</h6>
+                                        <h6>15h 15p</h6>
+                                        <h6>Viettinbank</h6>
+                                        <h6 className={"text-danger"}>Đã hủy</h6>
 
                                     </div>
                                 </div>
@@ -109,27 +164,10 @@ class PaymentHistory extends Component {
                                 <p className="text-danger">- 300.000 VND</p>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className={"col-md-8"}>
-                                <div className={"row"}>
-                                    <div className="col-md-4 text-right">
-                                        <i className="fab fa-500px"></i>
-                                    </div>
-                                    <div className="col-md-8">
-
-                                        <h6>Nang cap tai khoan vip</h6>
-                                        <p>15h 15p</p>
-                                        <p>Viettinbank</p>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={"col-md-4"}>
-                                <p className={"text-success"}>+ 600.000 VND</p>
-                            </div>
-                        </div>
                     </div>
                 </div>
+
+                <div className={"mt-4"}></div>
 
             </div>
         );
