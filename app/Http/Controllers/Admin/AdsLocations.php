e1abf26a6ed4e28b6b19cda70943ceb94ad724dc
vantu19000 <vantu19000@gmail.com>
2018-12-11 17:19:56 +0700

    Update AdsLocations.php

--- app/Http/Controllers/Admin/AdsLocations.php
+++ app/Http/Controllers/Admin/AdsLocations.php
@@ -40,4 +40,78 @@ class AdsLocations extends Controller
         return redirect($url)->with('success', [__('admin.SAVE_SUCCESS')]);
     }
 
+
+    public function migratehihi(){
+        $data = array(
+            [
+                'name' => 'Hẹn tốc độ 1',
+                'code' => 'DATING_ADS_1',
+                'parent_id' => 0,
+                'price' => 300000
+            ],
+            [
+                'name' => 'Hẹn tốc độ 2',
+                'code' => 'DATING_ADS_2',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+            [
+                'name' => 'Hẹn tốc độ 3',
+                'code' => 'DATING_ADS_3',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+            [
+                'name' => 'Module kết đôi 1',
+                'code' => 'COUPLE_ADS_1',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+            [
+                'name' => 'Module kết đôi 2',
+                'code' => 'COUPLE_ADS_2',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+            [
+                'name' => 'Trang like cá nhân 1',
+                'code' => 'LIKE_YOU_ADS_1',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+            [
+                'name' => 'Trang like cá nhân 2',
+                'code' => 'LIKE_YOU_ADS_2',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+            [
+                'name' => 'Trang like cá nhân 3',
+                'code' => 'LIKE_YOU_ADS_3',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+            [
+                'name' => 'Trang cà phê 1',
+                'code' => 'CAFE_ADS_1',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+            [
+                'name' => 'Trang cà phê 2',
+                'code' => 'CAFE_ADS_2',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+            [
+                'name' => 'Trang cà phê 3',
+                'code' => 'CAFE_ADS_3',
+                'parent_id' => 0,
+                'price' => 200000
+            ],
+
+        );
+        DB::table('ads_location')->insert($data);
+    }
+
 }
