ef7120f3b35611a5738e7ecb434ede5eaf65ed0e
vantu19000 <vantu19000@gmail.com>
2019-01-07 00:01:30 +0700

    ssss

--- resources/assets/js/actions/AddressActions.js
+++ resources/assets/js/actions/AddressActions.js
@@ -6,35 +6,42 @@ import {
 } from './types'
 
 export const getAllProvinces = () => (dispatch) => {
-    api.get('/provinces')
-        .then(response => {
-            dispatch({type: GET_ALL_PROVINCES, payload: response.data});
-        })
-        .catch(err => {
-            console.log(err);
-        })
+    return new Promise((resolve, reject) => {
+        api.get('/provinces')
+            .then(response => {
+                dispatch({type: GET_ALL_PROVINCES, payload: response.data});
+                resolve(response.data);
+            })
+            .catch(err => {
+                reject(err);
+            })
+    })
 }
 
 export const getAllDistricts = (province_id) => (dispatch) => {
     return new Promise((resolve, reject) => {
         api.get(`/districts/${province_id}`)
         .then(response => {
-            resolve(response.data);
             dispatch({type: GET_ALL_DISTRICTS, payload: response.data});
+            resolve(response.data);
         })
         .catch(err => {
             reject(err);
-            console.log(err);
         })
     })
 }
 
 export const getAllCommunes = (district_id) => (dispatch) => {
-    api.get(`/communes/${district_id}`)
-        .then(response => {
-            dispatch({type: GET_ALL_COMMUNES, payload: response.data});
-        })
-        .catch(err => {
-            console.log(err);
-        })
+    return new Promise((resolve, reject) => {
+        api.get(`/communes/${district_id}`)
+            .then(response => {
+                dispatch({type: GET_ALL_COMMUNES, payload: response.data});
+                resolve(response.data);
+            })
+            .catch(err => {
+                reject(err);
+
+            })
+
+    })
 }
--- resources/assets/js/routes/messages/filter.js
+++ resources/assets/js/routes/messages/filter.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {getJobs} from "../../actions/JobActions";
+import {getAllProvinces, getAllDistricts} from '../../actions/AddressActions';
 import {withRouter} from "react-router-dom";
 import {connect} from "react-redux";
+import Modal from "react-responsive-modal";
 
 class FilterTab extends Component {
 
@@ -10,9 +12,12 @@ class FilterTab extends Component {
         super(props);
 
         this.state = {
-            jobs: []
+            jobs: [],
+            provinces: [],
+            districts: [],
+            allDistricts: [],
+            open: false
         }
-
     }
 
     componentDidMount() {
@@ -22,11 +27,26 @@ class FilterTab extends Component {
                     jobs: result
                 })
             })
+        this.props.getAllProvinces()
+            .then(data => {
+                this.setState({
+                    provinces: data
+                })
+            })
+    }
+
+    loadDistrict(provinceID){
+        this.props.getAllDistricts(provinceID)
+            .then(data => {
+                this.setState({
+                    districts: data
+                })
+            })
     }
 
     render() {
 
-        const {jobs} = this.state;
+        const {jobs, provinces, districts, open} = this.state;
 
         return(
             <div className={"filter-box"}>
@@ -44,13 +64,68 @@ class FilterTab extends Component {
                     <label htmlFor="job"><i className="fa fa-briefcase"></i> Nghề nghiệp</label>
                     <select name="job_id" id="job" className={""}>
                         <option value="all">Tất cả</option>
-                    {
+                        {
                         jobs.map((item, i) => {
                             return(<option key={i} value={item.id}>{item.name}</option>);
                         })
-                    }
+                        }
                     </select>
                 </div>
+
+                <div className={"row"}>
+                    <div className={'col-md-6'}>
+                        <div className="form-group">
+                            <select name="province_id" id="province_id" onChange={(e) => {
+                                this.loadDistrict(e.target.value);
+                            }}>
+                                <option value="all">Tỉnh/TP</option>
+                                {
+                                    provinces.map((item, i) => {
+                                        return(
+                                            <option value={item.matp} key={i}>{item.name}</option>
+                                        )
+                                    })
+                                }
+                            </select>
+                        </div>
+                    </div>
+                    <div className={'col-md-6'}>
+                        <div className="form-group">
+                            <select name="province_id" id="province_id" className={""}>
+                                <option value="all">Quận/Huyện</option>
+                                {
+                                    districts.map((item, i) => {
+                                        return(
+                                            <option value={item.maqh} key={i}>{item.name}</option>
+                                        )
+                                    })
+                                }
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={'row'}>
+                    <div className={"col-md-4 offset-md-4"}>
+                        <button
+                            onClick={() => {
+                                this.setState({open: true})
+                            }}
+                            type="button"
+                            className="btn btn-primary btn-block"
+                        >Tìm kiếm</button>
+                    </div>
+                </div>
+
+
+                <Modal open={open} onClose={() => {this.setState({open: false})}} center>
+                    <h2>Danh sách</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                        pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
+                        hendrerit risus, sed porttitor quam.
+                    </p>
+                </Modal>
 
             </div>
         );
@@ -73,6 +148,8 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
     return {
         getJobs: () => dispatch(getJobs()),
+        getAllProvinces: () => dispatch(getAllProvinces()),
+        getAllDistricts: (provinceID) => dispatch(getAllDistricts(provinceID)),
     }
 }
 
