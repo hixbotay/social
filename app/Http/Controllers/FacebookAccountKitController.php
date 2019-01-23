3f80e25e8431547fa679c7ae0bf4c56560ddd676
zero2603 <20153713@student.hust.edu.vn>
2019-01-23 18:11:42 +0700

    fix bug

--- app/Http/Controllers/FacebookAccountKitController.php
+++ app/Http/Controllers/FacebookAccountKitController.php
@@ -11,25 +11,25 @@ class FacebookAccountKitController extends Controller
 
     public function endpoint(Request $request)
     {
-        // $code = $request->get('code');
-        // $client = new Client();
+        $code = $request->get('code');
+        $client = new Client();
 
-        // $url = "https://graph.accountkit.com/v1.2/access_token?grant_type=authorization_code&code="
-        //     .$code."&access_token=AA|".env('FACEBOOK_APP_ID')."|".env("ACCOUNTKIT_APP_SECRET");
+        $url = "https://graph.accountkit.com/v1.2/access_token?grant_type=authorization_code&code="
+            .$code."&access_token=AA|".env('FACEBOOK_APP_ID')."|".env("ACCOUNTKIT_APP_SECRET");
 
-        // $res = $client->get(rawurldecode($url));
-        // $data = json_decode($res->getBody());
+        $res = $client->get(rawurldecode($url));
+        $data = json_decode($res->getBody());
 
-        // $url_1 = "https://graph.accountkit.com/v1.2/me/?access_token=".$data->access_token;
-        // $phone_data = json_decode($client->get(rawurldecode($url_1))->getBody());
+        $url_1 = "https://graph.accountkit.com/v1.2/me/?access_token=".$data->access_token;
+        $phone_data = json_decode($client->get(rawurldecode($url_1))->getBody());
 
-        // $user = json_decode(Session::get('newUser'));
+        $user = json_decode(Session::get('newUser'));
 
-        // // print_r($phone_data->phone->national_number);
-        // $user->mobile = $phone_data->phone->national_number;
-        // $user->is_phone_verified = 1;
+        // print_r($phone_data->phone->national_number);
+        $user->mobile = $phone_data->phone->national_number;
+        $user->is_phone_verified = 1;
 
-        // return view('auth.fbauth', compact("user"));
-        return view('auth.fbauth');
+        return view('auth.fbauth', compact("user"));
+        // return view('auth.fbauth');
     }
 }
