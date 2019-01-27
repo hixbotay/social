14114231e9f04dc7c477cf4e62f97feac57087b9
vuong anh duong <vuonganhduong812@gmail.com>
2019-01-27 17:03:35 +0700

    update boi toan

--- boitoan/boitoan.php
+++ boitoan/boitoan.php
@@ -1,11 +1,16 @@
 <?php
-	
+
+require 'convertlunaryear.php';
 Class BoiToan{
 	static function get_year($date){
-		return (new DateTime($date))->format('Y');
+		$date = new DateTime($date);
+		
+		$date_converted = SunClass::convertSolar2Lunar($date->format('d'), $date->format('m'), $date->format('Y'));
+		
+		return date('Y',$date_converted);
 	}
 	
-	static function get_cung($birthday,$gender){
+	static function get_cung($birthday,$gender,$text = false){
 		$year = self::get_year($birthday);
 		$phan_du = array_sum(str_split($year)) % 9;
 		$cung_nam = array('1'=>'kham','2'=>'ly','3'=>'can', '4'=>'doai','5'=>'can_f',
@@ -17,25 +22,34 @@ Class BoiToan{
 		}else{
 			$cung = $cung_nu[$phan_du];
 		}
+		if($text){
+			return self::get_text('cung',$cung);
+		}
 		return $cung;
 	}
 	
-	static function get_can($birthday){
+	static function get_can($birthday, $text=false){
 		$year = self::get_year($birthday);
 		$last = $year[3];
 		$data = ['canh','tan','nham','quy','giap','at','binh','dinh','mau','ky'];
+		if($text){
+			return self::get_text('can',$data[$last]);
+		}
 		return $data[$last];
 	}
 	
-	static function get_chi($birthday){
+	static function get_chi($birthday,$text=false){
 		$year = self::get_year($birthday);
-		$last = ($year[2]+$year[3])%12;
-		$data = ['ti','suu','dan','mao','thin','ty','ngo','mui','than','dau','tuay','hoi'];
+		$last = (int)($year[2].$year[3])%12;
+		$data = ['ti','suu','dan','mao','thin','ty','ngo','mui','than','dau','tuat','hoi'];
+		if($text){
+			return self::get_text('chi',$data[$last]);
+		}
 		return $data[$last];
 	}
 	
-	static function get_can_chi($birthday){
-		return self::get_can($birthday).' '.self::get_chi($birthday);
+	static function get_can_chi($birthday,$text=false){
+		return self::get_can($birthday,$text).' '.self::get_chi($birthday,$text);
 	}
 	
 	static function get_thien_can($birthday){
@@ -45,21 +59,23 @@ Class BoiToan{
 	}
 	
 	static function get_dia_chi($birthday){
-		$can = self::get_can($birthday);
-		$data_thien_can = ['ty'=>1,'suu'=>'1','ngo'=>1,'mui'=>1,'dan'=>1,'mao'=>1,'than'=>1,'dau'=>1,'thin'=>2,'ty'=>2,'tuat'=>2,'hoi'=>2];
-		return $data_thien_can[$can];
+		$chi = self::get_chi($birthday);
+		$data_dia_chi = ['ty'=>0,'suu'=>0,'ngo'=>0,'mui'=>0,'dan'=>1,'mao'=>1,'than'=>1,'dau'=>1,'thin'=>2,'ty'=>2,'tuat'=>2,'hoi'=>2];
+		return $data_dia_chi[$chi];
 	}
 	
-	static function get_ngu_hanh($birthday){
+	static function get_ngu_hanh($birthday, $text = false){
 		$number = self::get_thien_can($birthday) + self::get_dia_chi($birthday);
 		$data = [1=>'kim','2'=>'thuy','3'=>'hoa','4'=>'tho','5'=>'moc'];
-		
-		$number = $number >5 ? $number-5 : $number;
+		$number = $number > 5 ? ($number-5) : $number;
+		if($text){
+			return self::get_text('ngu_hanh',$data[$number]);
+		}
 		return $data[$number];
 	}
 
 	static function get_menh($birthday){
-		$key = self::get_thien_can($birthday).'-'.self::get_dia_chi($birthday);
+		$key = self::get_can($birthday).'-'.self::get_chi($birthday);
 		return self::get_text('menh',$key);
 	}
 	
@@ -67,11 +83,25 @@ Class BoiToan{
 		return self::get_data()[$type][$value];
 	}
 
-	static function get_data(){
-		static $data_ngu_hanh;
-		if(!$data_ngu_hanh){
-			$data_ngu_hanh = json_decode(file_get_contents('data_ngu_hanh.json'),true);
+	static function get_data($key = null){
+		static $data_boi_toan;
+		if(!$data_boi_toan){
+			$data_boi_toan = json_decode(file_get_contents('data_boi_toan.json'),true);
 		}
-		return $data_ngu_hanh;
+		if($key){
+			return $data_boi_toan[$key];
+		}
+		return $data_boi_toan;
+	}
+
+	static function get_hoang_dao($birthday,$text = false){
+		$data_hoang_dao = self::get_data('cung_hoang_dao');
+		$date = date('m-d',$birthday);
+		foreach($data_hoang_dao as $cung){
+			if($date >= $cung->from && $date <= $cung->to){
+				return $cung;
+			}
+		}
+		return end($cung);
 	}
 }
--- boitoan/test.php
+++ boitoan/test.php
@@ -4,10 +4,11 @@ $nam_sinh=  $_GET['nam_sinh'];
 $gender = $_GET['gender'];
 require "boitoan.php";
 ?>
-get_cung: <?php echo BoiToan::get_text('cung',BoiToan::get_cung($nam_sinh,''))?><br>
+get_cung: <?php echo BoiToan::get_cung($nam_sinh,$gender,1)?><br>
 Can: <?php echo BoiToan::get_can($nam_sinh)?><br>
 Chi: <?php echo BoiToan::get_chi($nam_sinh)?><br>
 Thiên can: <?php echo BoiToan::get_thien_can($nam_sinh)?><br>
-Địa chi: <?php echo BoiToan::get_dia_chi($nam_sinh)?><br>
-Ngũ hành: <?php echo BoiToan::get_ngu_hanh($nam_sinh)?><br>
+Địa chi: <?php echo BoiToan::get_can_chi($nam_sinh,1)?><br>
+Ngũ hành: <?php echo BoiToan::get_ngu_hanh($nam_sinh,1)?><br>
 Menh: <?php echo BoiToan::get_menh($nam_sinh)?><br>
+Cung hoang dao: <?php echo BoiToan::get_hoang_dao($nam_sinh)->text_vi?><br>
