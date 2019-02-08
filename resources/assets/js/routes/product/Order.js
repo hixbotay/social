c511e9ffe7b2e3ed01c9930afbeabac0fef12fb8
zero2603 <20153713@student.hust.edu.vn>
2019-02-08 21:57:40 +0700

    Update Order.js

--- resources/assets/js/routes/product/Order.js
+++ resources/assets/js/routes/product/Order.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Table from "rc-table";
 import { getOrders } from '../../actions/ProductActions';
 import { connect } from 'react-redux';
 import { CardWithTitle } from '../../components/Card';
