8e4fb6738f151b0847db7aa7cda776f4ec9085cf
zero2603 <20153713@student.hust.edu.vn>
2019-03-10 14:34:43 +0700

    add canceled in event

---
+++ database/migrations/2019_03_10_073045_add_canceled_person_event.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCanceledPersonEvent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('events', function($table) {
+            $table->integer('canceled_person')->nullable(1);
+            $table->string('canceled_reason')->nullable(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
