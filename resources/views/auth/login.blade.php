36a6c83cc63272ff300f9eb9836ba383db972614
Dung <dungdt@zergitas.com>
2019-03-11 16:51:50 +0700

    ND-1: update link for login/register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.auth')
-
+@section('topmenu')
+    <ul class="nav float-right">
+        <li class="nav-item">
+            <a class="nav-link" href="{{url('/register')}}">Đăng ký</a>
+        </li>
+    </ul>
+@endsection
 @section('content')
 
 
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -16,51 +16,50 @@
 </head>
 <body>
     <div class="main-content authen-page">
-        <div class="row">
-            <div class="head-content">
-                <div class="logo float-left">
-                    <a href="{{url('/')}}">
-                    <img class="logo-size" src="{{asset('public/images/main/logo.png')}}" alt="">
-                    </a>
-                </div>
-                <ul class="nav float-right">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{url('/')}}">Đăng nhập</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <h1 class="page-title c-text">MẠNG XÃ HỘI HẸN HÒ THỰC TẾ</h1>
-            </div>
-            <div class="row">
-                <div class="col-xl-4 col-lg-4 col-md-12 col-xs-12"></div>
-                <div class="col-xl-4 col-lg-4 col-md-12 col-xs-12"></div>
-                <div class="col-xl-4 col-lg-4 col-md-12 col-xs-12 body-content">
-                    <h3 class="c-text">Tìm một nửa kia của bạn</h3>
-                    <h4 class="c-text description">Hãy tham gia ngay cùng cộng đồng noiduyen.vn để cùng gặp gỡ những người bạn mới.</h4>
-                    @yield('content')
-                    <div class="links">
-                        <span class="c-text">Hoặc sử dụng tài khoản thông qua</span> <a href="#"><img src="{{asset('public/images/main/fb.png')}}" width="37" height="37"></a>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="head-content">
+                        <div class="logo float-left">
+                            <a href="{{url('/')}}">
+                                <img class="logo-size" src="{{asset('public/images/main/logo.png')}}" height="50">
+                            </a>
+                        </div>
+                        @yield('topmenu')
                     </div>
-                    <div class="new-users">
-                        <h4 class="text-left pl-4 pb-3">Thành viên mới vừa đăng ký</h4>
-                        <div class="users pb-5 pl-xl-5 pl-lg-5 pl-md-5 pl-5 pr-xl-5 pr-lg-5 pr-md-5 pr-5">
-                            <div class="item pr-xl-4 pr-lg-4 pr-md-4 pr-4">
-                                <a href="/"><img src="{{asset('public/images/main/user.png')}}"></a>
-                            </div>
-                            <div class="item pr-xl-4 pr-lg-4 pr-md-4 pr-4">
-                                <a href="/"><img src="{{asset('public/images/main/user.png')}}"></a>
-                            </div>
-                            <div class="item pr-xl-4 pr-lg-4 pr-md-4 pr-4">
-                                <a href="/"><img src="{{asset('public/images/main/user.png')}}"></a>
+                </div>
+                <div class="row">
+                    <h1 class="page-title c-text">MẠNG XÃ HỘI HẸN HÒ THỰC TẾ</h1>
+                </div>
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 col-md-12 col-xs-12"></div>
+                    <div class="col-xl-4 col-lg-4 col-md-12 col-xs-12"></div>
+                    <div class="col-xl-4 col-lg-4 col-md-12 col-xs-12 body-content">
+                        <h5 class="c-text">Tìm một nửa kia của bạn</h5>
+                        <h6 class="c-text description">Hãy tham gia ngay cùng cộng đồng noiduyen.vn để cùng gặp gỡ những người bạn mới.</h6>
+                        @yield('content')
+                        <div class="links">
+                            <span class="c-text">Hoặc sử dụng tài khoản thông qua</span> <a href="{{url('/auth/facebook')}}"><img src="{{asset('public/images/main/fb.png')}}" width="37" height="37"></a>
+                        </div>
+                        <div class="new-users">
+                            <h5 class="text-left pl-4 pb-3">Thành viên mới vừa đăng ký</h5>
+                            <div class="users pb-5 pl-xl-5 pl-lg-5 pl-md-5 pl-5 pr-xl-5 pr-lg-5 pr-md-5 pr-5">
+                                <div class="item pr-xl-4 pr-lg-4 pr-md-4 pr-4">
+                                    <a href="/"><img src="{{asset('public/images/main/user.png')}}"></a>
+                                </div>
+                                <div class="item pr-xl-4 pr-lg-4 pr-md-4 pr-4">
+                                    <a href="/"><img src="{{asset('public/images/main/user.png')}}"></a>
+                                </div>
+                                <div class="item pr-xl-4 pr-lg-4 pr-md-4 pr-4">
+                                    <a href="/"><img src="{{asset('public/images/main/user.png')}}"></a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
     {{-- JS --}}
 <script src="{{asset('assets/js/jquery-2.1.4.min.js')}}"></script>
