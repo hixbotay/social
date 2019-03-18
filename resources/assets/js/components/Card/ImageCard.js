575ebbc5dfd88f006676096f423851075194d7ee
zero2603 <20153713@student.hust.edu.vn>
2019-03-18 22:51:35 +0700

    fix relationship

--- resources/assets/js/components/Card/ImageCard.js
+++ resources/assets/js/components/Card/ImageCard.js
@@ -27,16 +27,22 @@ class ImageCard extends Component {
                     data = {'is_loved': 0};
                     this.setState({isLoved: false, loveNumber: this.state.loveNumber - 1});
                 } else {
-                    data = {'is_loved': 1};
+                    data = {'is_loved': 1, 'is_like': 0};
                     this.setState({isLoved: true, loveNumber: this.state.loveNumber + 1});
+                    if(this.state.isLiked) {
+                        this.setState({isLiked: false, likeNumber: this.state.likeNumber - 1});
+                    }
                 }
             } else if(actionType == 'like') {
                 if(this.state.isLiked) {
                     data = {'is_like': 0};
                     this.setState({isLiked: false, likeNumber: this.state.likeNumber - 1});
                 } else {
-                    data = {'is_like': 1};
+                    data = {'is_loved': 0, 'is_like': 1};
                     this.setState({isLiked: true, likeNumber: this.state.likeNumber + 1});
+                    if(this.state.isLoved) {
+                        this.setState({isLoved: false, loveNumber: this.state.loveNumber - 1});
+                    }
                 }
             }
 
